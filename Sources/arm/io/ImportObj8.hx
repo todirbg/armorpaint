fee2c7975f9fcaff361d9db1071b5b7c48ef7df2
todirbg <todirbg@yahoo.com>
2022-02-12 00:14:30 +0200

    Imported obj-s names to TRIS index

--- Sources/arm/io/ImportObj8.hx
+++ Sources/arm/io/ImportObj8.hx
@@ -13,10 +13,10 @@ class ImportObj8 {
 
 			var obj = new Obj8Parser(b);
 			var filename = path.split("\\").pop().split("/").pop().split(".").shift();
-			obj.name = filename  + "." + obj.curObj;
+			obj.name = filename  + "." + obj.curObjName;
 			replaceExisting ? ImportMesh.makeMesh(obj, path) : ImportMesh.addMesh(obj);
 			while (obj.next()) {
-				obj.name = filename + "." + obj.curObj;
+				obj.name = filename + "." + obj.curObjName;
 				ImportMesh.addMesh(obj);
 			}
 			Data.deleteBlob(path);
